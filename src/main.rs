8dbc57638f94b57ca7ec633d9814c60a50c62fee
crumblingstatue <radiantstatue@gmail.com>
2023-02-07 17:07:47 +0100

    Move mdisk metadata into metadata module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod metadata;
+
 use std::path::PathBuf;
 
 use eframe::egui;
@@ -17,70 +19,6 @@ struct App {
     save_path: PathBuf,
     sav: Option<Sav>,
 }
-
-struct MdiskEntry {
-    bit_idx: u8,
-    name: &'static str,
-}
-
-const MDISK_TABLE: [MdiskEntry; 14] = [
-    MdiskEntry {
-        name: "Game Save",
-        bit_idx: 0,
-    },
-    MdiskEntry {
-        name: "Mr. De Castellac",
-        bit_idx: 3,
-    },
-    MdiskEntry {
-        name: "The pearl and the currents",
-        bit_idx: 2,
-    },
-    MdiskEntry {
-        name: "For Jade",
-        bit_idx: 4,
-    },
-    MdiskEntry {
-        name: "Surveillance Camera",
-        bit_idx: 11,
-    },
-    MdiskEntry {
-        name: "Hillyan Army Databank",
-        bit_idx: 1,
-    },
-    MdiskEntry {
-        name: "Beluga Checkup",
-        bit_idx: 10,
-    },
-    MdiskEntry {
-        name: "Animal Species",
-        bit_idx: 5,
-    },
-    MdiskEntry {
-        name: "Iris 511",
-        bit_idx: 6,
-    },
-    MdiskEntry {
-        name: "Iris 512",
-        bit_idx: 7,
-    },
-    MdiskEntry {
-        name: "Iris 513",
-        bit_idx: 8,
-    },
-    MdiskEntry {
-        name: "Iris 514",
-        bit_idx: 9,
-    },
-    MdiskEntry {
-        name: "Disk Game",
-        bit_idx: 12,
-    },
-    MdiskEntry {
-        name: "Pearl Game",
-        bit_idx: 13,
-    },
-];
 
 impl App {
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
@@ -114,7 +52,7 @@ impl eframe::App for App {
             });
             if let Some(sav) = &mut self.sav {
                 ui.separator();
-                for en in MDISK_TABLE {
+                for en in metadata::MDISK_TABLE {
                     ui.checkbox(&mut sav.mdisks.disks[en.bit_idx as usize], en.name);
                 }
                 ui.separator();
